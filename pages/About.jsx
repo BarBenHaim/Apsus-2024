e97b6a281d9742a5d5ccfb12e9a9c91bbc7f1fd1
Bar Ben Haim <barbenbh@gmail.com>
2024-06-26 10:07:09 +0300

    commit

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,6 +1,7 @@
-
 export function About() {
-    return <section className="about">
-        <h1>About Page</h1>
-    </section>
+    return (
+        <section className='about'>
+            <h1>About Page</h1>
+        </section>
+    )
 }
