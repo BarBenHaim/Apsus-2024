b57ff07590fc7a23b8dcc1597f4278e45c165204
Nir Fakiro <fakironir@gmail.com>
2024-06-27 19:01:00 +0300

    Add style

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,12 +1,34 @@
 import { MailDetails } from './MailDetails.jsx'
 
 export function MailPreview({ mail, onRemoveMail, onStar }) {
-  function getCurrentDate() {
+  function getRelativeTime(date) {
     const now = new Date()
-    const day = now.getDate().toString().padStart(2, '0')
-    const month = (now.getMonth() + 1).toString().padStart(2, '0') // Month is zero-indexed
-    const year = now.getFullYear()
-    return `${day}/${month}/${year}`
+    const diff = now - new Date(date) // Difference in milliseconds
+
+    const seconds = Math.floor(diff / 1000)
+    const minutes = Math.floor(seconds / 60)
+    const hours = Math.floor(minutes / 60)
+    const days = Math.floor(hours / 24)
+
+    if (days > 5) {
+      const dateObj = new Date(date)
+      const day = dateObj.getDate().toString().padStart(2, '0')
+      const month = (dateObj.getMonth() + 1).toString().padStart(2, '0')
+      const year = dateObj.getFullYear()
+      return `${day}/${month}/${year}`
+    }
+
+    if (days < 1) {
+      const dateObj = new Date(date)
+      const hours = dateObj.getHours().toString().padStart(2, '0')
+      const minutes = dateObj.getMinutes().toString().padStart(2, '0')
+      return `${hours}:${minutes}`
+    }
+
+    if (seconds < 60) return `${seconds} seconds ago`
+    if (minutes < 60) return `${minutes} minutes ago`
+    if (hours < 24) return `${hours} hours ago`
+    return `${days} days ago`
   }
   const isRead = mail.isRead
   return (
@@ -24,8 +46,10 @@ export function MailPreview({ mail, onRemoveMail, onStar }) {
           {mail.from}
         </p>
         <p>{mail.subject}</p>
-        <p>{getCurrentDate()}</p>
-        <section>
+        <p className="time">{getRelativeTime(mail.createdAt)}</p>
+        <section className="preview-btn">
+          <i className="fa-regular fa-share-from-square"></i>
+          <i className="fa-regular fa-envelope-open"></i>
           <i
             onClick={(ev) => onRemoveMail(ev, mail.id)}
             className="fa-regular fa-trash-can"
